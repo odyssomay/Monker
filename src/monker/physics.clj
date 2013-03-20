e24036cc19882da61454a0a69dfdbc799608663a
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-03-20 17:55:56 +0100

    initial bullet-app-state

--- src/monker/physics.clj
+++ src/monker/physics.clj
@@ -1,1 +1,22 @@
-(ns monker.physics)
+(ns monker.physics
+  (:use [monker.util :only [Configurable
+                            conf-int configure-helper]])
+  (:import (com.jme3.bullet
+             BulletAppState
+             BulletAppState$ThreadingType)))
+
+(extend-type BulletAppState
+  Configurable
+  (configure [s params]
+    (configure-helper
+      params param
+      :threading
+      (case param
+        :sequential
+        BulletAppState$ThreadingType/SEQUENTIAL
+        :parallel
+        BulletAppState$ThreadingType/PARALLEL))))
+
+(defn bullet-app-state []
+  (let [b (BulletAppState.)]
+    b))
