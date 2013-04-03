dcd84c404ad08abf60d859c0136d609fa96cb78b
Jonathan Fischer Friberg <odyssomay@gmail.com>
2013-04-03 21:08:39 +0200

    type hints

--- src/monker/ui.clj
+++ src/monker/ui.clj
@@ -3,14 +3,16 @@
             (monker.ui
               [element :as element]
               [style :as style]))
-  (:import de.lessvoid.nifty.Nifty
+  (:import (de.lessvoid.nifty.builder
+             ScreenBuilder)
+           de.lessvoid.nifty.Nifty
            com.jme3.niftygui.NiftyJmeDisplay
            com.jme3.app.Application))
 
 ;; =====
 ;; Nifty
 ;; =====
-(defn nifty-display
+(defn ^NiftyJmeDisplay nifty-display
   "Create a NiftyJmeDisplay
   and attach it to the application.
   "
@@ -29,7 +31,7 @@
       nifty-display)
     :else (util/convert-err obj)))
 
-(defn nifty
+(defn ^Nifty nifty
   "Get a Nifty object."
   {:arglists '([nifty] [nifty-display])}
   [obj]
@@ -54,7 +56,7 @@
         nifty-display (nifty-display obj)
         nifty (nifty nifty-display)]
     (doseq [screen screens]
-      (let [e (element/into-element screen)
+      (let [e ^ScreenBuilder (element/into-element screen)
             built (.build e nifty)]
         (.addScreen nifty (.getScreenId built) built)))
     (.gotoScreen nifty (or (name start-screen) "start"))
--- src/monker/ui/effect.clj
+++ src/monker/ui/effect.clj
@@ -11,6 +11,6 @@
       (doseq [[k v] param]
         (.effectParameter this k v)))))
 
-(defn effect [effect & {:as options}]
+(defn ^EffectBuilder effect [effect & {:as options}]
   (let [effect (util/dash-to-camel (name effect))]
     (util/conf-int (EffectBuilder. effect) options)))
